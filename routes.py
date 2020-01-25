0976293b94b27dedb81274095eb2590eb62b0f74
Cosima <cs.manughian@gmail.com>
2020-01-24 16:00:20 -0800

    added a fun sentiment analysis API route

--- routes.py
+++ routes.py
@@ -4,7 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return 'welcome to the Arrow API >>>------|>'
+    return 'welcome to the Arrow API\n>------|>'
+
+@app.route('/analyze/<text>')
+def analyze(text):
+    print(text)
+    import random
+    mood = random.choice(['happy', 'sad', 'angry', 'upset'])
+    sentence = random.choice([
+        "Wow! You're really {} today!",
+        "Huh... You seem pretty {}.",
+        "Geez! No need to be so {}!",
+        "You're totally {}. Why? You should write about it using Arrow!"
+    ])
+    return sentence.format(mood)
 
 if __name__ == '__main__':
     app.run(debug=True)
